4e1459d37f1113dff794409b205d354a3c4b706b
ajmalbinabdulaziz <ajmalbinabdulaziz@gmail.com>
2021-12-19 22:09:41 +0530

    configured ghost cms backend

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,33 @@
-// import Head from 'next/head'
-// import Image from 'next/image'
-// import styles from '../styles/Home.module.css'
+import { getPosts } from "../lib/functions"
+import Link from "next/link"
 
-export default function Home() {
-  return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-  )
+
+
+const IndexPage = (props) => (
+  <ul>
+    {props.posts.map((post) => (
+      <li key={post.id}>
+        <Link href={`/posts/${post.slug}`}>
+          <a>{post.title}</a>
+        </Link>
+      </li>
+    ))}
+  </ul>
+);
+
+export default IndexPage;
+
+export async function getStaticProps(context) {
+  const posts = await getPosts();
+
+  if (!posts) {
+    return {
+      notFound: true,
+    };
+  }
+
+  return {
+    props: { posts },
+    revalidate: 1,
+  };
 }
-
